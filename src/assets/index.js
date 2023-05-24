3fdc039c2ad1d2488cd8110d49a0f88117834341
Gonza <gonzagramaglia@gmail.com>
2023-05-24 01:22:26 -0300

    Skills

--- src/assets/index.js
+++ src/assets/index.js
@@ -21,10 +21,8 @@ import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
 
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
+import scrimba from "./company/scrimba.png";
+import freecodecamp from "./company/freecodecamp.png";
 
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
@@ -52,10 +50,8 @@ export {
   tailwind,
   typescript,
   threejs,
-  meta,
-  shopify,
-  starbucks,
-  tesla,
+  scrimba,
+  freecodecamp,
   carrent,
   jobit,
   tripguide,
--- src/constants/index.js
+++ src/constants/index.js
@@ -15,10 +15,8 @@ import {
     git,
     figma,
     docker,
-    meta,
-    starbucks,
-    tesla,
-    shopify,
+    scrimba,
+    freecodecamp,
     carrent,
     jobit,
     tripguide,
@@ -31,8 +29,8 @@ import {
       title: "About",
     },
     {
-      id: "work",
-      title: "Work",
+      id: "skills",
+      title: "Skills",
     },
     {
       id: "contact",
@@ -114,57 +112,55 @@ import {
     },
   ];
   
-  const experiences = [
+  const skills = [
     {
-      title: "React.js Developer",
-      company_name: "Starbucks",
-      icon: starbucks,
-      iconBg: "#383E56",
-      date: "March 2020 - April 2021",
+      title: "Next JS",
+      company_name: "freeCodeCamp",
+      icon: freecodecamp,
+      iconBg: "#0b0a25",
+      date: "June 2023",
       points: [
-        "Developing and maintaining web applications using React.js and other related technologies.",
-        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-        "Implementing responsive design and ensuring cross-browser compatibility.",
-        "Participating in code reviews and providing constructive feedback to other developers.",
+
       ],
     },
     {
-      title: "React Native Developer",
-      company_name: "Tesla",
-      icon: tesla,
-      iconBg: "#E6DEDD",
-      date: "Jan 2021 - Feb 2022",
+      title: "Frontend Developer Career Path",
+      company_name: "Scrimba",
+      icon: scrimba,
+      iconBg: "#0b0a25",
+      date: "March 2023 - June 2023",
       points: [
-        "Developing and maintaining web applications using React.js and other related technologies.",
-        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-        "Implementing responsive design and ensuring cross-browser compatibility.",
-        "Participating in code reviews and providing constructive feedback to other developers.",
+        "HTML 5","CSS 3", "Responsive design","Advanced JavaScript", "Working with APIs","Advanced React JS"
       ],
     },
     {
-      title: "Web Developer",
-      company_name: "Shopify",
-      icon: shopify,
-      iconBg: "#383E56",
-      date: "Jan 2022 - Jan 2023",
+      title: "React Router v6",
+      company_name: "Scrimba",
+      icon: scrimba,
+      iconBg: "#0b0a25",
+      date: "May 2021 - May 2022",
       points: [
-        "Developing and maintaining web applications using React.js and other related technologies.",
-        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-        "Implementing responsive design and ensuring cross-browser compatibility.",
-        "Participating in code reviews and providing constructive feedback to other developers.",
+        
       ],
     },
     {
-      title: "Full stack Developer",
-      company_name: "Meta",
-      icon: meta,
-      iconBg: "#E6DEDD",
-      date: "Jan 2023 - Present",
+      title: "TypeScript",
+      company_name: "Scrimba",
+      icon: scrimba,
+      iconBg: "#0b0a25",
+      date: "Apr 2022 - Apr 2023",
       points: [
-        "Developing and maintaining web applications using React.js and other related technologies.",
-        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-        "Implementing responsive design and ensuring cross-browser compatibility.",
-        "Participating in code reviews and providing constructive feedback to other developers.",
+
+      ],
+    },
+    {
+      title: "JavaScript Programming",
+      company_name: "freeCodeCamp",
+      icon: freecodecamp,
+      iconBg: "#0b0a25",
+      date: "Feb 2023 - Feb 2023",
+      points: [
+
       ],
     },
   ];
@@ -262,4 +258,4 @@ import {
     },
   ];
   
-  export { services, technologies, experiences, testimonials, projects };
+  export { services, technologies, skills, testimonials, projects };
