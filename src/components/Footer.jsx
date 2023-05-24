ad000c33697221f0f6558b6d9e6b7141ef2019be
Gonza <gonzagramaglia@gmail.com>
2023-05-24 10:22:26 -0300

    Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
+
+const Footer = () => {
+  return (
+    <footer className="bg-primary text-white py-12">
+      <div className="container mx-auto px-4">
+        <div className="flex justify-center items-center gap-5">
+          <a
+            href="https://www.linkedin.com/in/gonzagramaglia/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-secondary hover:text-white mx-2"
+          >
+            <FaLinkedin size={28} />
+          </a>
+          <a
+            href="https://github.com/gonzagramaglia"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-secondary hover:text-white mx-2"
+          >
+            <FaGithub size={28} />
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/components/index.js
+++ src/components/index.js
@@ -6,6 +6,7 @@ import Tech from './Tech';
 import Skills from './Skills';
 import Works from './Works';
 import Contact from './Contact';
+import Footer from './Footer'
 
 export {
   Hero,
@@ -18,5 +19,6 @@ export {
   EarthCanvas, 
   BallCanvas, 
   ComputersCanvas, 
-  StarsCanvas
+  StarsCanvas,
+  Footer
 }
