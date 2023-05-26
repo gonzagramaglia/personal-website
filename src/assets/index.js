dceac4f1187a065af7ea5b77651abd15b3030afe
Gonza <gonzagramaglia@gmail.com>
2023-05-26 03:01:55 -0300

    JavaScript Mastery

--- src/assets/index.js
+++ src/assets/index.js
@@ -21,7 +21,7 @@ import threejs from "./tech/threejs.svg";
 
 import scrimba from "./company/scrimba.png";
 import freecodecamp from "./company/freecodecamp.png";
-import jsmastery from "./company/jsmastery.png"
+import jsmastery from "./company/jsmastery.png";
 
 import notes from "./notes.png";
 import memesgenerator from "./memesgenerator.png";
