5d5eb211348e84e65b82f026bf10b70970ec029e
Gonza <gonzagramaglia@gmail.com>
2023-06-23 13:21:15 -0300

    Sizing

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -125,7 +125,7 @@ const Contact = () => {
 
       <motion.div
         variants={slideIn("right", "tween", 0.2, 1)}
-        className='xl:flex-1 xl:h-auto md:h-[400px] h-[200px]'
+        className='xl:flex-1 xl:h-auto md:h-[470px] h-[250px]'
       >
         <EarthCanvas />
       </motion.div>
